9f4a065ff8ead37f37e9dbeb4ee09f99178fa6c0
anzzz999 <995127051@qq.com>
2021-02-20 16:57:19 +0800

    多线程断点测试

--- src/main/java/test/TestDemo.java
+++ src/main/java/test/TestDemo.java
@@ -1,20 +1,40 @@
 package test;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
+/**
+ * 多线程断点调试
+ *
+ * 断点上直接右键设置：把 All 改为 Thread。如果点了 Make Default ，那么后续加上的断点都是 Thead 设置，之前加上的断点不影响。
+ */
 public class TestDemo {
     public static void main(String[] args) {
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread1 = new Thread(myRunnable, "线程1");
+        Thread thread2 = new Thread(myRunnable, "线程2");
+        Thread thread3 = new Thread(myRunnable, "线程3");
 
-    new ThreadLocal<>();
-
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
 
+    static class MyRunnable implements Runnable {
 
-     public class ListNode {
-         int val;
-         ListNode next;
-         ListNode() {}
-         ListNode(int val) { this.val = val; }
-         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     }
+        @Override
+        public void run() {
+            Thread currentThread = Thread.currentThread();
+            System.out.println(currentThread.getName() + "-------------进入");
+
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(currentThread.getName() + "-------------离开");
+            }
+
+        }
+    }
 }
